6a53a953f3327c42eb8b6c0d0ee7823f8a163c14
Danylo Nechyporchuk <danylo.nechyporchuk@gmail.com>
2022-07-25 23:38:29 +0300

    Real btc-uah rate
    Return json with bitcoin rate in uah.
    Key for rate is "rate".
    Value for rate integer value.
    
    Send 400 error if something goes wrong on service side

--- btc_service.go
+++ btc_service.go
@@ -3,19 +3,40 @@ package main
 import (
 	"encoding/json"
 	"github.com/gorilla/mux"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
 
 type rate struct {
-	BtcRate int `json:"rate"`
+	UahRate int `json:"rate"`
 }
 
 func getRateBTC(w http.ResponseWriter, r *http.Request) {
-	tmpRate := 800000
+	//Get json with bitcoin rate from third-party service
+	response, getRateError := http.Get("https://api.coinstats.app/public/v1/coins?skip=0&limit=1&currency=UAH")
+	if getRateError != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	defer response.Body.Close()
 
-	err := json.NewEncoder(w).Encode(rate{tmpRate})
-	if err != nil {
+	//Read json from response
+	body, readJsonError := ioutil.ReadAll(response.Body)
+	if readJsonError != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	//Parse json from third-party service
+	var coins map[string][]map[string]float64
+	json.Unmarshal(body, &coins)
+	rateBtcUah := int(coins["coins"][0]["price"])
+
+	//Write response with btc-uah rate
+	writeResponseError := json.NewEncoder(w).Encode(rate{UahRate: rateBtcUah})
+	if writeResponseError != nil {
+		w.WriteHeader(http.StatusBadRequest)
 		return
 	}
 }

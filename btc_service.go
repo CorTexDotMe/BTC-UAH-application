ce33fd971e3d74f5680699517f302e6b8e90ac6b
Danylo Nechyporchuk <danylo.nechyporchuk@gmail.com>
2022-07-26 15:01:16 +0300

    Save emails with /subscribe
    Emails are saved in emails.txt file in project directory.
    Each line in file represent one email.
    
    Send 409 error if email already subscribed(in file emails.txt)

--- btc_service.go
+++ btc_service.go
@@ -1,18 +1,31 @@
 package main
 
 import (
+	"bufio"
 	"encoding/json"
 	"github.com/gorilla/mux"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 )
 
-func getRateBTC(w http.ResponseWriter, r *http.Request) {
+const emailsFileName = "emails.txt"
+
+func main() {
+	router := mux.NewRouter()
+	apiRouter := router.Host("gses2.app").PathPrefix("/api").Subrouter()
+	apiRouter.HandleFunc("/rate", getRateBTC).Methods("GET")
+	apiRouter.HandleFunc("/subscribe", subscribeEmail).Methods("POST")
+
+	log.Fatal(http.ListenAndServe(":80", apiRouter))
+}
+
+func getRateBTC(writer http.ResponseWriter, request *http.Request) {
 	//Get json with bitcoin rate from third-party service
 	response, getRateError := http.Get("https://api.coinstats.app/public/v1/coins?skip=0&limit=1&currency=UAH")
 	if getRateError != nil {
-		w.WriteHeader(http.StatusBadRequest)
+		writer.WriteHeader(http.StatusBadRequest)
 		return
 	}
 	defer response.Body.Close()
@@ -20,7 +33,7 @@ func getRateBTC(w http.ResponseWriter, r *http.Request) {
 	//Read json from response
 	body, readJsonError := ioutil.ReadAll(response.Body)
 	if readJsonError != nil {
-		w.WriteHeader(http.StatusBadRequest)
+		writer.WriteHeader(http.StatusBadRequest)
 		return
 	}
 
@@ -30,17 +43,47 @@ func getRateBTC(w http.ResponseWriter, r *http.Request) {
 	rateBtcUah := int(coins["coins"][0]["price"])
 
 	//Write response with btc-uah rate
-	writeResponseError := json.NewEncoder(w).Encode(rateBtcUah)
+	writeResponseError := json.NewEncoder(writer).Encode(rateBtcUah)
 	if writeResponseError != nil {
-		w.WriteHeader(http.StatusBadRequest)
+		writer.WriteHeader(http.StatusBadRequest)
 		return
 	}
 }
 
-func main() {
-	router := mux.NewRouter()
-	apiRouter := router.Host("gses2.app").PathPrefix("/api").Subrouter()
-	apiRouter.HandleFunc("/rate", getRateBTC).Methods("GET")
+func subscribeEmail(writer http.ResponseWriter, request *http.Request) {
+	parsingError := request.ParseForm()
+	if parsingError != nil {
+		writer.WriteHeader(http.StatusConflict)
+		return
+	}
 
-	log.Fatal(http.ListenAndServe(":80", apiRouter))
+	success := addEmail(request.Form.Get("email"))
+	if !success {
+		writer.WriteHeader(http.StatusConflict)
+		return
+	}
+}
+
+func addEmail(email string) bool {
+	file, openingError := os.OpenFile(emailsFileName, os.O_RDWR|os.O_CREATE, 0600)
+	if openingError != nil {
+		return false
+	}
+	defer file.Close()
+
+	fileScanner := bufio.NewScanner(file)
+	for fileScanner.Scan() {
+		if fileScanner.Text() == email {
+			return false
+		}
+	}
+	if fileScanner.Err() != nil {
+		return false
+	}
+
+	if _, writingError := file.WriteString(email + "\n"); writingError != nil {
+		return false
+	}
+
+	return true
 }

6fa4c9c672ac9212438d15bdc594f162934265c4
Danylo Nechyporchuk <danylo.nechyporchuk@gmail.com>
2022-09-10 13:54:10 +0300

    Using environment variables during email testing
    mailsurp api key from .env file

--- test/services/email/email_test.go
+++ test/services/email/email_test.go
@@ -5,35 +5,16 @@ import (
 	"btcApp/test/utils"
 	"fmt"
 	"github.com/antihax/optional"
+	"github.com/joho/godotenv"
 	mailslurp "github.com/mailslurp/mailslurp-client-go"
 	"golang.org/x/net/context"
+	"os"
+	"path/filepath"
 	"testing"
 )
 
-func GetMailClient() (*mailslurp.APIClient, context.Context) {
-	emailContext := context.WithValue(
-		context.Background(),
-		mailslurp.ContextAPIKey,
-		mailslurp.APIKey{Key: utils.ServiceForTestingMailingApiKey},
-	)
-
-	config := mailslurp.NewConfiguration()
-	client := mailslurp.NewAPIClient(config)
-
-	return client, emailContext
-}
-
-func CreateInbox(t *testing.T, client *mailslurp.APIClient, emailContext context.Context) *mailslurp.InboxDto {
-	inbox, _, err := client.InboxControllerApi.CreateInbox(emailContext, nil)
-
-	if err != nil {
-		utils.Failure(t, "Error during creating an inbox: %s", err)
-	}
-	return &inbox
-}
-
 func TestSendEmail(t *testing.T) {
-	client, emailContext := GetMailClient()
+	client, emailContext := GetMailClient(t)
 	inbox := CreateInbox(t, client, emailContext)
 
 	testData := 10000
@@ -54,4 +35,47 @@ func TestSendEmail(t *testing.T) {
 	} else {
 		utils.Failure(t, "Received message is wrong. Actual: %s, expected: %s", *receivedEmail.Body, expectedMessage)
 	}
+}
+
+func GetMailClient(t *testing.T) (*mailslurp.APIClient, context.Context) {
+	loadEnvironment(t)
+
+	emailContext := context.WithValue(
+		context.Background(),
+		mailslurp.ContextAPIKey,
+		mailslurp.APIKey{Key: os.Getenv("MAILSLURP_API_KEY")},
+	)
+
+	config := mailslurp.NewConfiguration()
+	client := mailslurp.NewAPIClient(config)
+
+	return client, emailContext
+}
+
+func loadEnvironment(t *testing.T) {
+	projectDirectory := findProjectDirectoryWithEnvFile()
+
+	loadingError := godotenv.Load(projectDirectory + "/.env")
+	if loadingError != nil {
+		utils.Failure(t, "Environment not loaded")
+	}
+}
+
+func findProjectDirectoryWithEnvFile() string {
+	currentDirectory, _ := os.Getwd()
+	var projectDirectory string
+	for i := 0; i < 3; i++ {
+		projectDirectory = filepath.Dir(currentDirectory)
+		currentDirectory = projectDirectory
+	}
+	return projectDirectory
+}
+
+func CreateInbox(t *testing.T, client *mailslurp.APIClient, emailContext context.Context) *mailslurp.InboxDto {
+	inbox, _, err := client.InboxControllerApi.CreateInbox(emailContext, nil)
+
+	if err != nil {
+		utils.Failure(t, "Error during creating an inbox: %s", err)
+	}
+	return &inbox
 }

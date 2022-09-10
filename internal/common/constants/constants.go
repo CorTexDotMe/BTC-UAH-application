4a659e0d5cc4cf7a760f3c713e22f3bec92c0997
Danylo Nechyporchuk <danylo.nechyporchuk@gmail.com>
2022-09-10 14:28:53 +0300

    Created database has its own directory now

--- internal/common/constants/constants.go
+++ internal/common/constants/constants.go
@@ -5,7 +5,7 @@ const (
 	BtcUahRateServiceUrl = "https://api.coinstats.app/public/v1/coins?skip=0&limit=1&currency=UAH"
 
 	EmailsFileName = "emails.txt"
-	EmailsFilePath = "internal/repository/resources"
+	EmailsFilePath = "internal/repository/resources/"
 
 	Success = "\u2713"
 	Failed  = "\u2717"
